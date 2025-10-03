83e722619e015a3114c9c7fc97fb954f4a6bbc3b
fausto-romay <fromay972@gmail.com>
2025-10-03 19:55:23 -0300

    Estructura común del index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Rutas:
+const userRoute = require("./routes/userRoute");
+
+// Base de datos (completar):
+const dataBase = require("")["sequelize"];
+
+// Para ejecutar:
+app.use(express.json());
+app.use("/user", userRoute);
+
+// Listen (no se si esta bien):
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+
+    console.log(`App iniciada en el puerto ${PORT}`);
+})
